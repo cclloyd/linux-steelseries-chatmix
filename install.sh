81727aa5236eb7940b36c84b0fdca9cb0aeb039a
cclloyd <cclloyd@cclloyd.com>
2025-12-19 19:06:49 -0500

    Updated install script

--- install.sh
+++ install.sh
@@ -6,37 +6,25 @@
 #     https://github.com/birdybirdonline
 #     https://github.com/awth13
 
-echo "Starting..."
-
 if [ "$(id -u)" -ne 0 ]; then
     echo "Please run the install script as root."
     exit 1
 fi
 
-# TODO: fix installing from curl
-# TODO: Add command args to chatmix (these are run as user)
-#     - install/uninstall: will install udev and systemd
-#     - start/stop: shortcuts for systemd commands
-# TODO: 
-# TODO:
-# TODO:
-
-
-echo "Installing Arctis 7+ ChatMix."
-SCRIPT="chatmix.py"
+echo "Installing Arctis 7 ChatMix"
 SCRIPT_DIR="/usr/local/bin/"
 
 if [ -f "chatmix.py" ]; then # Check if binary is present and install manually
-    echo "Installing script to ${SCRIPT_DIR}${SCRIPT}."
-    cp "$SCRIPT" "$SCRIPT_DIR"
-    # TODO: remove extension
+    echo "Installing script to ${SCRIPT_DIR}chatmix."
+    cp "chatmix.py" "${SCRIPT_DIR}chatmix"
 else
     echo "Installing from GitHub..."
     curl -L -o "${SCRIPT_DIR}chatmix" "https://raw.githubusercontent.com/cclloyd/linux-steelseries-chatmix/refs/heads/main/chatmix.py"
+    chmod +x "${SCRIPT_DIR}chatmix"
     if [ $? -eq 0 ]; then
         echo "Successfully downloaded chatmix.py to ${SCRIPT_DIR}"
     else
-        echo "Failed to download install.sh"
+        echo "Failed to download install.sh. Try cloning the repo and running locally instead."
         exit 1
     fi
 fi
